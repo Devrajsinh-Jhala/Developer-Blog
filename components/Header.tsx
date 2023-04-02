fd2b8025e94aeec2a41384409a9ca084f0b9be4b
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2023-04-02 16:32:31 +0530

    Silly error...

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,15 +11,6 @@ const Header = (props: Props) => {
   return (
     <header className="mx-auto w-full">
       <Navbar />
-      <img
-        src={
-          session
-            ? session?.user?.image
-            : "https://media.licdn.com/dms/image/C4D03AQGzOhPWOqYTQA/profile-displayphoto-shrink_200_200/0/1655304743647?e=1686182400&v=beta&t=5Qd7iGEGNE35PpEOo8VjEZJrJdwAxnpordgbgQsoqsE"
-        }
-        alt=""
-        className="h-8 w-8 rounded-full object-contain"
-      />
       {session ? (
         <button onClick={() => signOut()}>Sign Out</button>
       ) : (
