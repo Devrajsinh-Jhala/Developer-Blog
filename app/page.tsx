13f9ee9d71cb6e289fa9d37b2a077c3a3b040b4e
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2023-04-02 16:50:26 +0530

    Silly error...

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import React from "react";
 import BlogList from "../components/BlogList";
 import { sanityClient } from "../sanity";
 
-type Props = {};
-
 const query = groq`
 *[_type == 'post']{
   ...,
@@ -13,7 +11,7 @@ const query = groq`
 } | order(_createdAt desc)
 `;
 
-const Home = async (props: Props) => {
+const Home = async () => {
   const posts = await sanityClient.fetch(query);
   return (
     <>
